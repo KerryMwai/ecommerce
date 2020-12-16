695b34b6b6626b9f5e1fea772274a9924b914873
Kerry Mwai <kerrywangamati@gmail.com>
2020-12-16 09:26:50 +0300

    header,footer,index,welcome and layout page

--- resources/views/ecommerce/header.blade.php
+++ resources/views/ecommerce/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,10 +15,10 @@
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-info" type="submit">Search</button>
       </form>
       <form class="d-flex">
-        <button class="btn btn-outline-success ml-4" type="submit">Cart(0)</button>
+        <button class="btn btn-outline-info ml-4" type="submit">Cart(0)</button>
       </form>
     </div>
   </div>
